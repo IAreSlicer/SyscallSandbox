5b7a4e84e40009e75ca7c3d7d15a7d64da51978d
Daan Bentsnijder <d.b.bentsnijder@student.tudelft.nl>
2025-12-05 18:10:12 +0100

    Add function so that every program gets own policy file

--- tracer/tracer.cpp
+++ tracer/tracer.cpp
@@ -1,64 +1,83 @@
-    #include <sys/ptrace.h>
-    #include <sys/types.h>
-    #include <sys/wait.h>
-    #include <sys/user.h>
-    #include <unistd.h>
-    #include <iostream>
-    #include <set>
-    #include <fstream>  
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <unistd.h>
+#include <iostream>
+#include <set>
+#include <fstream>  
+#include <sstream>
+#include <string>
 
-    using namespace std;
+using namespace std;
 
-    int main(int argc, char* argv[]) {
-        if (argc < 2) {  
-            return -1;
+std::string get_basename(const std::string& path) {
+    size_t pos = path.find_last_of("/\\");
+    if (pos == std::string::npos) return path;
+    return path.substr(pos + 1);
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {  
+        cerr << "Usage: ./tracer <program> [args...]" << endl;
+        return 1;
+    }
+    
+    pid_t child = fork();
+    set<int> syscalls;
+    
+    if (child == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (child == 0) {
+        //Traced Child
+        if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) == -1) {
+            perror("ptrace");
+            exit(EXIT_FAILURE);
+        }
+        execvp(argv[1], &argv[1]); 
+        exit(127);
+    // Tracer Process
+    } else {
+        int status;
+        
+        // Wait for child to stop on its first instruction
+        if (waitpid(child, &status, 0) == -1) {
+            perror("waitpid");
+            return 1;
         }
         
-        pid_t child = fork();
-        set<int> syscalls;
+        // Set options to trace system calls
+        ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
         
-        // Traced process
-        if (child == 0) {
-            ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
-            execvp(argv[1], &argv[1]); 
-            exit(1);
-        // Tracer Process
-        } else {
-            int status;
-            
-            // Wait for child to stop on its first instruction
+        while (true) {
+            // Hold at the next syscall
+            ptrace(PTRACE_SYSCALL, child, 0, 0);
             waitpid(child, &status, 0);
             
-            // Set options to trace system calls
-            ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
-            
-            while (true) {
-                // Hold at the next syscall
-                ptrace(PTRACE_SYSCALL, child, 0, 0);
-                waitpid(child, &status, 0);
-                
-                if (WIFEXITED(status)) {
-                    // Program has exited
-                    break;
-                }
-                
-                struct user_regs_struct regs;
-                ptrace(PTRACE_GETREGS, child, 0, &regs);
-                
-                // On linux x86_64, syscall number is in orig_rax
-                int syscall_num = regs.orig_rax;
-                syscalls.insert(syscall_num);
+            if (WIFEXITED(status) || WIFSIGNALED(status)) {
+                // Program has exited
+                break;
             }
+            
+            struct user_regs_struct regs;
+            ptrace(PTRACE_GETREGS, child, 0, &regs);
+            
+            // On linux x86_64, syscall number is in orig_rax
+            int syscall_num = regs.orig_rax;
+            syscalls.insert(syscall_num);
         }
+    }
         
-        
-   const char* output_file = "policy_syscalls.txt";
+    // Set file name to be safe
+    string prog = get_basename(argv[1]);
+    string output_file = "policy_syscalls_" + prog + ".txt";
 
     // Read syscalls from the file
-    std::set<int> all_syscalls;
-
+    set<int> all_syscalls;
     {
-        std::ifstream in(output_file);
+        ifstream in(output_file);
         int num;
 
         // If the file does not exist yet, the loop does nothing
@@ -74,17 +93,15 @@
 
     // Write the union back to the file (overwrite old content)
     {
-        std::ofstream out(output_file, std::ios::trunc);
+        ofstream out(output_file, ios::trunc);
         if (!out) {
-            std::cerr << "Failed to open " << output_file << " for writing\n";
+            cerr << "Failed to open " << output_file << " for writing" << endl;
             return 1;
         }
 
         for (int syscall_num : all_syscalls) {
-            out << syscall_num << '\n';
+            out << syscall_num << endl;
         }
     }
-
-        
-        return 0;
-    }
+    return 0;
+}
